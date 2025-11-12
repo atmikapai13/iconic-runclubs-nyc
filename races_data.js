23e322421518fb41b1044b4c0ad5f1b28d30f983
atmikapai13 <atmikapai@berkeley.edu>
2025-11-12 13:07:08 -0500

    added big marathons

---
+++ races_data.js
@@ -0,0 +1,168 @@
+// NYC Races Data - Iconic races and marathons in New York City
+var races = [
+    {
+        RaceName: "TCS New York City Marathon",
+        Date: "November 3, 2024",
+        Location: "Staten Island (Start) / Central Park (Finish)",
+        Address: "Fort Wadsworth, Staten Island, NY",
+        Distance: "26.2 miles",
+        Borough: "Multi-Borough",
+        RegistrationURL: "https://www.nyrr.org/tcsnycmarathon",
+        latitude: 40.6062,
+        longitude: -74.0550
+    },
+    {
+        RaceName: "United Airlines NYC Half",
+        Date: "March 16, 2025",
+        Location: "Brooklyn (Start) / Central Park (Finish)",
+        Address: "Prospect Park, Brooklyn, NY",
+        Distance: "13.1 miles",
+        Borough: "Multi-Borough",
+        RegistrationURL: "https://www.nyrr.org/races/united-airlines-nyc-half",
+        latitude: 40.6602,
+        longitude: -73.9690
+    },
+    {
+        RaceName: "Brooklyn Half Marathon",
+        Date: "May 17, 2025",
+        Location: "Prospect Park",
+        Address: "Prospect Park, Brooklyn, NY",
+        Distance: "13.1 miles",
+        Borough: "Brooklyn",
+        RegistrationURL: "https://www.nyrr.org/races/brooklynhalf",
+        latitude: 40.6602,
+        longitude: -73.9690
+    },
+    {
+        RaceName: "Percy Sutton Harlem 5K Run",
+        Date: "August 2025",
+        Location: "Marcus Garvey Park",
+        Address: "18 Mount Morris Park W, New York, NY",
+        Distance: "5K",
+        Borough: "Manhattan",
+        RegistrationURL: "https://www.nyrr.org/races/percysuttonharlem5k",
+        latitude: 40.8030,
+        longitude: -73.9420
+    },
+    {
+        RaceName: "NYRR Dash to the Finish Line 5K",
+        Date: "November 2, 2024",
+        Location: "Central Park",
+        Address: "Central Park West & 72nd St, New York, NY",
+        Distance: "5K",
+        Borough: "Manhattan",
+        RegistrationURL: "https://www.nyrr.org/races/dashtofinish",
+        latitude: 40.7764,
+        longitude: -73.9740
+    },
+    {
+        RaceName: "NYCRUNS Brooklyn Bridge Run",
+        Date: "April 2025",
+        Location: "Brooklyn Bridge Park",
+        Address: "Brooklyn Bridge Park, Brooklyn, NY",
+        Distance: "10K/5K",
+        Borough: "Brooklyn",
+        RegistrationURL: "https://nycruns.com/races/brooklyn-bridge-run/",
+        latitude: 40.7024,
+        longitude: -73.9875
+    },
+    {
+        RaceName: "NYCRUNS Governors Island Races",
+        Date: "June 2025",
+        Location: "Governors Island",
+        Address: "Governors Island, New York, NY",
+        Distance: "10K/5K",
+        Borough: "Manhattan",
+        RegistrationURL: "https://nycruns.com/races/governors-island/",
+        latitude: 40.6892,
+        longitude: -74.0177
+    },
+    {
+        RaceName: "NYCRUNS Roosevelt Island Run",
+        Date: "September 2025",
+        Location: "Roosevelt Island",
+        Address: "Roosevelt Island, New York, NY",
+        Distance: "10K/5K",
+        Borough: "Manhattan",
+        RegistrationURL: "https://nycruns.com/races/roosevelt-island/",
+        latitude: 40.7614,
+        longitude: -73.9510
+    },
+    {
+        RaceName: "Staten Island Half Marathon",
+        Date: "October 2025",
+        Location: "Midland Beach",
+        Address: "Midland Beach, Staten Island, NY",
+        Distance: "13.1 miles",
+        Borough: "Staten Island",
+        RegistrationURL: "https://nycruns.com/races/staten-island-half/",
+        latitude: 40.5734,
+        longitude: -74.0854
+    },
+    {
+        RaceName: "NYRR Queens 10K",
+        Date: "April 2025",
+        Location: "Flushing Meadows Corona Park",
+        Address: "Flushing Meadows Corona Park, Queens, NY",
+        Distance: "10K",
+        Borough: "Queens",
+        RegistrationURL: "https://www.nyrr.org/races/queens10k",
+        latitude: 40.7400,
+        longitude: -73.8450
+    },
+    {
+        RaceName: "NYCRUNS Bronx 10 Miler",
+        Date: "October 2025",
+        Location: "Van Cortlandt Park",
+        Address: "Van Cortlandt Park, Bronx, NY",
+        Distance: "10 miles",
+        Borough: "Bronx",
+        RegistrationURL: "https://nycruns.com/races/bronx-10-miler/",
+        latitude: 40.8988,
+        longitude: -73.8862
+    },
+    {
+        RaceName: "NYRR Fred Lebow Manhattan Half",
+        Date: "January 2025",
+        Location: "Central Park",
+        Address: "Central Park, New York, NY",
+        Distance: "13.1 miles",
+        Borough: "Manhattan",
+        RegistrationURL: "https://www.nyrr.org/races/fredlebowmanhattanhalf",
+        latitude: 40.7829,
+        longitude: -73.9654
+    },
+    {
+        RaceName: "NYCRUNS Gantry Plaza 5K",
+        Date: "July 2025",
+        Location: "Gantry Plaza State Park",
+        Address: "Gantry Plaza State Park, Queens, NY",
+        Distance: "5K",
+        Borough: "Queens",
+        RegistrationURL: "https://nycruns.com/races/gantry-plaza/",
+        latitude: 40.7445,
+        longitude: -73.9585
+    },
+    {
+        RaceName: "Brooklyn Marathon",
+        Date: "April 2025",
+        Location: "Prospect Park",
+        Address: "Prospect Park, Brooklyn, NY",
+        Distance: "26.2 miles",
+        Borough: "Brooklyn",
+        RegistrationURL: "https://nycruns.com/races/brooklyn-marathon/",
+        latitude: 40.6602,
+        longitude: -73.9690
+    },
+    {
+        RaceName: "NYRR 5th Avenue Mile",
+        Date: "September 2025",
+        Location: "5th Avenue",
+        Address: "5th Avenue & 80th St, New York, NY",
+        Distance: "1 mile",
+        Borough: "Manhattan",
+        RegistrationURL: "https://www.nyrr.org/races/5thavenuemile",
+        latitude: 40.7794,
+        longitude: -73.9632
+    }
+];
